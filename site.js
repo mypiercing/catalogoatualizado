283a897297b50522a575e7b8b31a514ecf6de885
mypiercing <134015288+mypiercing@users.noreply.github.com>
2024-03-27 13:10:27 -0300

    Update site.js

--- site.js
+++ site.js
@@ -349,22 +349,24 @@ for (var i = 0; i < colorLargeButtons.length; i++) {
     colorLargeButtons[i].addEventListener("click", openPopup);
 }
 
-    function getCartItemsText() {
-      var cartElements = document.getElementById("cart").children;
-      var cartText = "";
-      for (var i = 0; i < cartElements.length; i++) {
-          let productInfo = cartElements[i].querySelector(".product-info").innerText;
-          // We find the last line which contains the price calculation.
-          let lastLineStart = productInfo.lastIndexOf("\n") + 1;
-          let productText = productInfo.substring(0, lastLineStart);
-          let productPriceLine = productInfo.substring(lastLineStart);
-          // Now we can separate the price part to apply the bold effect
-          let productPriceParts = productPriceLine.split("=");
-          let productPrice = productPriceParts[0] + "=*" + productPriceParts[1].trim() + "*";
-          cartText += productText + productPrice + "\n\n";
-      }
-      return cartText;
+function getCartItemsText() {
+  var cartElements = document.getElementById("cart").children;
+  var cartText = "";
+  for (var i = 0; i < cartElements.length; i++) {
+      let productInfo = cartElements[i].querySelector(".product-info").innerText;
+      // We find the last line which contains the price calculation.
+      let lastLineStart = productInfo.lastIndexOf("\n") + 1;
+      let productText = productInfo.substring(0, lastLineStart);
+      let productPriceLine = productInfo.substring(lastLineStart);
+      // Now we can separate the price part to apply the bold effect
+      let productPriceParts = productPriceLine.split("=");
+      // Add a space between the "=" and the "*" for WhatsApp formatting
+      let productPrice = productPriceParts[0] + "= *" + productPriceParts[1].trim() + "*";
+      cartText += productText + productPrice + "\n\n";
   }
+  return cartText;
+}
+
   
     document.getElementById("popup-close").addEventListener("click", closePopup);
     document.getElementById("whatsappButton").addEventListener("click", function () {
